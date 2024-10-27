9dbcf10612ab921d3c2c1dcb9a6ed6f57df7a782
gianr <gianr@DESKTOP-B9MCHEE>
2024-10-26 21:14:40 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRUDAPI.Data;
+using CRUDAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductosController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public ProductosController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Producto>>> GetProductos()
+    {
+        return await _context.Productos.ToListAsync();
+    }
+
+ 
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Producto>> GetProducto(int id)
+    {
+        var producto = await _context.Productos.FindAsync(id);
+        if (producto == null)
+        {
+            return NotFound();
+        }
+        return producto;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Producto>> PostProducto(Producto producto)
+    {
+        _context.Productos.Add(producto);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetProducto), new { id = producto.Id }, producto);
+    }
+
+  
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutProducto(int id, Producto producto)
+    {
+        if (id != producto.Id)
+        {
+            return BadRequest();
+        }
+        _context.Entry(producto).State = EntityState.Modified;
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!ProductoExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+        return NoContent();
+    }
+
+   
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProducto(int id)
+    {
+        var producto = await _context.Productos.FindAsync(id);
+        if (producto == null)
+        {
+            return NotFound();
+        }
+        _context.Productos.Remove(producto);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    private bool ProductoExists(int id)
+    {
+        return _context.Productos.Any(e => e.Id == id);
+    }
+}
---
+++ DATA/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CRUDAPI.Models;
+
+namespace CRUDAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
+
+
